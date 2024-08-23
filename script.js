da6fc438db1cef1ac5d65a4138754e4a881b33f8
KhoiNguyen213 <khoinguyendang151209@gmail.com>
2024-08-23 21:09:25 +0700

    update login and register

--- script.js
+++ script.js
@@ -247,6 +247,57 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const username = document.getElementById("username");
+
+const btnRegister = document.getElementById("btnRegister");
+
+if (btnRegister) {
+  btnRegister.addEventListener("click", () => {
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then(async (userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        await sendEmailVerification(user);
+        await updateProfile(auth.currentUser, {
+          displayName: username.value,
+        });
+        await signOut(auth);
+        alert("Please verify your email");
+        location.href = "./login.html";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        alert(errorCode);
+      });
+  });
+}
+
+// const btnLogin = document.getElementById("btnLogin");
+// if (btnLogin) {
+//   btnLogin.addEventListener("click", function () {
+//     signInWithEmailAndPassword(auth, email.value, password.value)
+//       .then(async (userCredential) => {
+//         const user = userCredential.user;
+//         if (!user.emailVerified) {
+//           signOut(auth);
+//           throw { code: "Email not verified", message: "" };
+//         }
+//       })
+//       .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         console.log(errorCode, errorMessage);
+//         alert(errorCode);
+//       });
+//   });
+// }
 const querySnapshot = await getDocs(collection(db, "product"));
 querySnapshot.forEach((doc) => {
   let product = doc.data();
